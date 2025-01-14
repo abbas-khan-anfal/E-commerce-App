2b57cb98b93a8f973b682e8b6cc6ee288d49cc74
Abbas Khan <ak9950025@gmail.com>
2025-01-14 19:05:35 +0500

    minor changes updated

--- backend/config/MongoDB.js
+++ backend/config/MongoDB.js
@@ -1,1 +1,12 @@
-lkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelklkdjflekfjelkfjelk
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb+srv://mydemoforuser:demo500account25@cluster0.updwn.mongodb.net/ecommerce?retryWrites=true&w=majority');
+        console.log('DB Connected Successfully');
+    } catch (error) {
+        console.log('Error Connecting DB:', error);
+    }
+};
+
+export default connectDB;
