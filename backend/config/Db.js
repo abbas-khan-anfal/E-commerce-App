cdc75f955277fc469007a0c3484a82ae1f6aa57a
Abbas Khan <ak9950025@gmail.com>
2025-07-10 20:58:43 +0500

    Db.js file updated

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://mydemoforuser:demo500account25@cluster0.updwn.mongodb.net/ecommerce?retryWrites=true&w=majority');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('DB Connected Successfully');
     } catch (error) {
         console.log('Error Connecting DB:', error);
