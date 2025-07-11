96e3e7574ee1c3931895b7902a8af723f8b15784
Abbas Khan <ak9950025@gmail.com>
2025-07-12 01:27:43 +0500

    function udpated

--- backend/utils/AdminCookie.js
+++ backend/utils/AdminCookie.js
@@ -4,8 +4,10 @@ const adminCookie = (admin, res, message, statusCode = 200) => {
     const adminToken = jwt.sign({ admin_Id : admin._id }, process.env.JWT_SECRET)
 
     res.status(statusCode).cookie('adminToken', adminToken, {
-        httpOnly : true,
-        maxAge : 15 * 24 * 60 * 60 * 1000 // expire after 15 days
+        httpOnly: true,
+        secure: true, // Required for HTTPS
+        sameSite: 'None', // Required for cross-site cookies
+        maxAge: 15 * 24 * 60 * 60 * 1000,
     })
     .json({
         success : true,
--- backend/utils/UserCookie.js
+++ backend/utils/UserCookie.js
@@ -4,8 +4,10 @@ const userCookie = (user, res, message, statusCode = 200) => {
     const userToken = jwt.sign({ user_Id : user._id }, process.env.JWT_SECRET)
 
     res.status(statusCode).cookie('userToken', userToken, {
-        httpOnly : true,
-        maxAge : 15 * 24 * 60 * 60 * 1000 // expire after 15 days
+        httpOnly: true,
+        secure: true, // Required for HTTPS
+        sameSite: 'None', // Required for cross-site cookies
+        maxAge: 15 * 24 * 60 * 60 * 1000, // expire after 15 days
     })
     .json({
         success : true,
